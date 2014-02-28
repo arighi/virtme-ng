2ccb91491dc1adbe631340acd7018b32fcd3826e
Andy Lutomirski <luto@amacapital.net>
2014-02-27 23:51:07 -0700

    Change the mount tag of the root to virtme.root.
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>

--- mkinitramfs.py
+++ mkinitramfs.py
@@ -64,7 +64,7 @@ source /modules/load_all.sh
 
 echo 'Mounting hostfs...'
 
-if ! /bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any hostroot /newroot/; then
+if ! /bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any virtme.root /newroot/; then
   echo "Failed to switch to real root.  We are stuck."
   sleep 5
   exit 1
