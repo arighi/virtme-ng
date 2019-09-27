016ee911312511b2ca895f545047f7334f3c0c48
Andy Lutomirski <luto@kernel.org>
2019-09-27 10:21:26 -0700

    kmod: Improve logic to find and run virtme-prep-kdir-mods
    Signed-off-by: Andy Lutomirski <luto@kernel.org>

--- virtme/guest_tools.py
+++ virtme/guest_tools.py
@@ -1,14 +1,16 @@
 # -*- mode: python -*-
 # resources.py: Find virtme's resources
-# Copyright © 2014 Andy Lutomirski
+# Copyright © 2014-2019 Andy Lutomirski
 # Licensed under the GPLv2, which is available in the virtme distribution
 # as a file called LICENSE with SHA-256 hash:
 # 8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643
 
-"""Helpers to find virtme's guest tools."""
+"""Helpers to find virtme's guest tools and host scripts."""
 
 import os
+import shutil
 import pkg_resources
+import subprocess
 
 def find_guest_tools():
     """Return the path of the guest tools installed with the running virtme.
@@ -19,3 +21,22 @@ def find_guest_tools():
 
     # No luck.  This is somewhat surprising.
     return None
+
+def find_script(name):
+    # If we're running out of a source checkout, we can find scripts through
+    # the 'virtme/scripts' symlink.
+    fn = pkg_resources.resource_filename(__name__, 'scripts/%s' % name)
+    if os.path.isfile(fn):
+        return fn
+
+    # Otherwise assume we're actually installed and in PATH.
+    fn = shutil.which(name)
+    if fn is not None:
+        return fn
+
+    # No luck.  This is somewhat surprising.
+    raise Exception('could not find script %s' % name)
+
+def run_script(name):
+    fn = find_script(name)
+    subprocess.check_call(executable=fn, args=[fn])
