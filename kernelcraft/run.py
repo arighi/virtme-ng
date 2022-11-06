19c1b4dfa7ab134fd3bba075e8bb79b9c7ef9cf2
Andrea Righi <andrea.righi@canonical.com>
2022-11-06 16:06:57 +0100

    initial version
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

---
+++ kernelcraft/run.py
@@ -0,0 +1,129 @@
+import argparse
+import os
+import sys
+import socket
+import shutil
+import subprocess
+from pathlib import Path
+
+KERNEL_DIR = str()
+
+def make_parser():
+    parser = argparse.ArgumentParser(
+        description='Craft and test a specific kernel',
+    )
+
+    parser.add_argument('--release', '-r', action='store', metavar='RELEASE',
+            help='Use a kernel from a specific Ubuntu release or upstream')
+
+    parser.add_argument('--commit', '-c', action='store', metavar='COMMIT',
+            help='Use a kernel identified by a specific commit id or tag (default is HEAD)')
+
+    parser.add_argument('--opts', '-o', action='store', metavar='OPTS',
+            help='Additional options passed to virtme-run')
+
+    parser.add_argument('--resume', '-u', action='store_true',
+            help='Start the previosly generated kernel')
+
+    return parser
+
+_ARGPARSER = make_parser()
+
+def arg_fail(message):
+    print(message)
+    _ARGPARSER.print_usage()
+    exit(1)
+
+UBUNTU_RELEASE = {
+    # Upstream kernels
+    'mainline': {
+        'repo': 'git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git',
+        'branch': 'master',
+    },
+    'next': {
+        'repo': 'git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git',
+        'branch': 'master',
+    },
+    'stable': {
+        'repo': 'git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git',
+        'branch': 'master',
+    },
+
+    # Ubuntu kernels
+    'ubuntu-unstable': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/unstable',
+        'branch': 'master',
+    },
+    'ubuntu-kinetic': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/kinetic',
+        'branch': 'master-next',
+    },
+    'ubuntu-jammy': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy',
+        'branch': 'master-next',
+    },
+    'ubuntu-focal': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/focal',
+        'branch': 'master-next',
+    },
+    'ubuntu-bionic': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/bionic',
+        'branch': 'master-next',
+    },
+    'ubuntu-xenial': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial',
+        'branch': 'master-next',
+    },
+    'ubuntu-trusty': {
+        'repo': 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/trusty',
+        'branch': 'master-next',
+    },
+}
+
+class KernelSource:
+    def __init__(self, srcdir):
+        if not os.path.exists(srcdir):
+           os.makedirs(srcdir)
+           os.chdir(srcdir)
+           subprocess.check_call(['git', 'init'])
+        self.cpus = str(os.cpu_count())
+        os.chdir(srcdir)
+        self.srcdir = srcdir
+
+    def checkout(self, release, commit=None):
+        if not release in UBUNTU_RELEASE:
+            sys.stderr.write(f"ERROR: unknown release {release}\n")
+        if subprocess.call(['git', 'remote', 'get-url', release], stderr=subprocess.DEVNULL):
+            repo_url = UBUNTU_RELEASE[release]['repo']
+            subprocess.check_call(['git', 'remote', 'add', release, repo_url])
+        subprocess.check_call(['git', 'fetch', release])
+        target = commit or (release + '/' + UBUNTU_RELEASE[release]['branch'])
+        subprocess.check_call(['git', 'reset', '--hard', target])
+
+    def config(self):
+        subprocess.check_call(['virtme-configkernel', '--defconfig'])
+
+    def make(self):
+        subprocess.check_call(['make', '-j', self.cpus])
+
+    def run(self):
+        hostname = socket.gethostname()
+        username = os.getlogin()
+        # Disable kaslr because it may break debugging tools
+        opts = '-a nokaslr'
+        # Start VM using virtme
+        cmd = f'virtme-run --name {hostname} --kdir {self.srcdir} --mods auto -a nokaslr --user {username} --qemu-opts -m 4096 -smp {self.cpus} -s -qmp tcp:localhost:3636,server,nowait'
+        subprocess.check_call([str(c) for c in cmd.split(' ')])
+
+def main():
+    args = _ARGPARSER.parse_args()
+
+    ks = KernelSource(str(Path.home()) + '/.kernelcraft')
+    if not args.resume:
+        ks.checkout(args.release, args.commit)
+        ks.config()
+        ks.make()
+    ks.run()
+
+if __name__ == '__main__':
+    exit(main())
--- setup.py
+++ setup.py
@@ -4,40 +4,28 @@ import os
 import sys
 from setuptools import setup
 
-if sys.version_info < (3,8):
-    print('virtme requires Python 3.8 or higher')
+if sys.version_info < (3,3):
+    print('kernelcraft requires Python 3.3 or higher')
     sys.exit(1)
 
-_VERSION = '0.1.2'
+_VERSION = '0.1'
 
 setup(
-    name='virtme',
+    name='kernelcraft',
     version=_VERSION,
-    author='Andy Lutomirski',
-    author_email='luto@amacapital.net',
-    description='Virtualize the running distro or a simple rootfs',
-    url='https://git.kernel.org/cgit/utils/kernel/virtme/virtme.git',
-    download_url='https://www.kernel.org/pub/linux/utils/kernel/virtme/releases/virtme-%s.tar.xz' % _VERSION,
+    author='Andrea Righi',
+    author_email='andrea.righi@canonical.com',
+    description='',
+    url='https://git.launchpad.net/~arighi/+git/kernelcraft',
     license='GPLv2',
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.md'), 'r').read(),
-    packages=['virtme', 'virtme.commands', 'virtme.guest'],
+    packages=['kernelcraft'],
     install_requires=[],
     entry_points = {
         'console_scripts': [
-            'virtme-run = virtme.commands.run:main',
-            'virtme-configkernel = virtme.commands.configkernel:main',
-            'virtme-mkinitramfs = virtme.commands.mkinitramfs:main',
+            'kc = kernelcraft.run:main',
         ]
-    },
-    scripts = [
-        'bin/virtme-prep-kdir-mods',
-        ],
-    package_data = {
-        'virtme.guest': [
-            'virtme-init',
-            'virtme-udhcpc-script',
-        ],
     },
     classifiers=['Environment :: Console',
                  'Intended Audience :: Developers',
@@ -45,8 +33,4 @@ setup(
                  'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
                  'Operating System :: POSIX :: Linux',
              ],
-
-    # virtme actually does work as a zip, but that forces the guest
-    # tools to get extracted at run time.
-    zip_safe = False,
 )
