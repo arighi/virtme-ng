279530210b656c36dfa1391baa22c1248636c830
Andrea Righi <andrea.righi@canonical.com>
2022-11-30 11:57:30 +0100

    version 0.5
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- kernelcraft/run.py
+++ kernelcraft/run.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from shutil import copyfile
 from argcomplete import autocomplete
 
-VERSION = '0.4'
+VERSION = '0.5'
 
 def make_parser():
     parser = argparse.ArgumentParser(
