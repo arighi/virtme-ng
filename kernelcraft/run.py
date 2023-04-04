arighi/virtme-ng

commit 244d66e5c61285ee1328675869d7e997fb2194cc

setup: Install kernelcraft.conf and update the references
Also, rename version.py to utils.py, since it now contains the conf_path
and conf_file variables. We don't need the pkg_resource lib anymore,
since the file will always be installed in the user .config directory.

Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

kernelcraft/run.py
@@ -3,15 +3,13 @@ import os
 import sys
 import socket
 import shutil
-import pkg_resources
 import json
 import tempfile
 from subprocess import call, check_call, check_output, DEVNULL
 from pathlib import Path
-from shutil import copyfile
 from argcomplete import autocomplete
 
-from kernelcraft.version import VERSION
+from kernelcraft.utils import VERSION, conf_file
 
 def make_parser():
     parser = argparse.ArgumentParser(
@@ -165,11 +163,10 @@ class KernelSource:
         if not os.path.isdir('.git'):
             arg_fail('error: must run from a kernel git repository', show_usage=False)
         # Initialize known kernels
-        conf = str(Path.home()) + '/.kernelcraft.conf'
-        if not Path(conf).exists():
-            default_conf = pkg_resources.resource_filename('kernelcraft', '../etc/kernelcraft.conf')
-            copyfile(default_conf, conf)
-        with open(conf) as fd:
+        if not conf_file.exists():
+            sys.stderr.write("ERROR: Missing {conf_file}\n")
+            sys.exit(1)
+        with open(conf_file) as fd:
             conf_data = json.loads(fd.read())
             if 'repo' in conf_data:
                 self.kernel_release = conf_data['repo']

kernelcraft/utils.py
@@ -1,0 +1,5 @@
+from pathlib import Path
+
+VERSION = '1.8'
+conf_path = Path(Path.home(), '.config', 'kernelcraft')
+conf_file = Path(conf_path, 'kernelcraft.conf')

kernelcraft/version.py
@@ -1,1 +1,0 @@
-VERSION = '1.8'

setup.py
@@ -3,7 +3,7 @@
 import os
 import sys
 from setuptools import setup
-from kernelcraft.version import VERSION
+from kernelcraft.utils import VERSION, conf_path
 
 if sys.version_info < (3,3):
     print('kernelcraft requires Python 3.3 or higher')
@@ -29,7 +29,7 @@ setup(
         ]
     },
     data_files = [
-        ('/etc', ['cfg/kernelcraft.conf']),
+        (str(conf_path), ['cfg/kernelcraft.conf']),
     ],
     classifiers=['Environment :: Console',
                  'Intended Audience :: Developers',

commit 60c9d6acc697d182c9bcace57190159d473a5778

run: fix missing f-string in error message
Fixes: 244d66e ("setup: Install kernelcraft.conf and update the references")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

kernelcraft/run.py
@@ -164,7 +164,7 @@ class KernelSource:
             arg_fail('error: must run from a kernel git repository', show_usage=False)
         # Initialize known kernels
         if not conf_file.exists():
-            sys.stderr.write("ERROR: Missing {conf_file}\n")
+            sys.stderr.write(f"ERROR: Missing {conf_file}\n")
             sys.exit(1)
         with open(conf_file) as fd:
             conf_data = json.loads(fd.read())