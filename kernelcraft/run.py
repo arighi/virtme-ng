21a9b78a0a5765d2d4dcaa342367d2a780371626
Andrea Righi <andrea.righi@canonical.com>
2023-04-04 08:12:05 +0200

    config: drop /etc/kernelcraft.conf and always use config in $HOME
    There is no reason to create a file in /etc/kernelcraft.conf and also
    check for a ~/.kernelcraft.conf, we can always check for the latter and
    only use this.
    
    This fixes issue #3.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- kernelcraft/run.py
+++ kernelcraft/run.py
@@ -167,9 +167,7 @@ class KernelSource:
         # Initialize known kernels
         conf = str(Path.home()) + '/.kernelcraft.conf'
         if not Path(conf).exists():
-            default_conf = '/etc/kernelcraft.conf'
-            if not Path(default_conf).exists():
-                default_conf = pkg_resources.resource_filename('kernelcraft', '../etc/kernelcraft.conf')
+            default_conf = pkg_resources.resource_filename('kernelcraft', '../etc/kernelcraft.conf')
             copyfile(default_conf, conf)
         with open(conf) as fd:
             self.kernel_release = json.loads(fd.read())
