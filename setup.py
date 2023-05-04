arighi/virtme-ng

commit 244d66e5c61285ee1328675869d7e997fb2194cc

setup: Install kernelcraft.conf and update the references
Also, rename version.py to utils.py, since it now contains the conf_path
and conf_file variables. We don't need the pkg_resource lib anymore,
since the file will always be installed in the user .config directory.

Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

setup.py
@@ -3,7 +3,7 @@
 import os
 import sys
 from setuptools import setup
-from kernelcraft.version import VERSION
+from kernelcraft.utils import VERSION, conf_path
 
 if sys.version_info < (3,3):
     print('kernelcraft requires Python 3.3 or higher')
@@ -29,7 +29,7 @@ setup(
         ]
     },
     data_files = [
-        ('/etc', ['cfg/kernelcraft.conf']),
+        (str(conf_path), ['cfg/kernelcraft.conf']),
     ],
     classifiers=['Environment :: Console',
                  'Intended Audience :: Developers',

kernelcraft/run.py
@@ -3,15 +3,13 @@ import os
 import sys
 import socket
 import shutil
-import pkg_resources
 import json
 import tempfile
 from subprocess import call, check_call, check_output, DEVNULL
 from pathlib import Path
-from shutil import copyfile
 from argcomplete import autocomplete
 
-from kernelcraft.version import VERSION
+from kernelcraft.utils import VERSION, conf_file
 
 def make_parser():
     parser = argparse.ArgumentParser(
@@ -165,11 +163,10 @@ class KernelSource:
         if not os.path.isdir('.git'):
             arg_fail('error: must run from a kernel git repository', show_usage=False)
         # Initialize known kernels
-        conf = str(Path.home()) + '/.kernelcraft.conf'
-        if not Path(conf).exists():
-            default_conf = pkg_resources.resource_filename('kernelcraft', '../etc/kernelcraft.conf')
-            copyfile(default_conf, conf)
-        with open(conf) as fd:
+        if not conf_file.exists():
+            sys.stderr.write("ERROR: Missing {conf_file}\n")
+            sys.exit(1)
+        with open(conf_file) as fd:
             conf_data = json.loads(fd.read())
             if 'repo' in conf_data:
                 self.kernel_release = conf_data['repo']

kernelcraft/utils.py
@@ -1,0 +1,5 @@
+from pathlib import Path
+
+VERSION = '1.8'
+conf_path = Path(Path.home(), '.config', 'kernelcraft')
+conf_file = Path(conf_path, 'kernelcraft.conf')

kernelcraft/version.py
@@ -1,1 +1,0 @@
-VERSION = '1.8'

commit 03a8f70d158be6d260f6b390329d65dd8362193a

setup.py: install default config file in the proper location
Default configuration file should be installed in /etc when provide the
deb package and eventually a local configuration can be defined in
~/.config/virtme-ng/virtme-ng.conf.

Fixes: 244d66e ("setup: Install kernelcraft.conf and update the references")
Reported-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com>
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

setup.py
@@ -31,7 +31,7 @@ setup(
         ]
     },
     data_files = [
-        (str(CONF_PATH), ['cfg/virtme-ng.conf']),
+        ('/etc', ['cfg/virtme-ng.conf'])
     ],
     scripts = [
         'bin/virtme-prep-kdir-mods',