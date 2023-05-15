ae15bc0cc1d7ba267ba005c8481ce37a04bd3cad
Andrea Righi <andrea.righi@canonical.com>
2023-05-15 11:51:58 +0200

    rename the last pieces of kernelcraft to virtme-ng
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 import sys
 from setuptools import setup
-from kernelcraft.utils import VERSION, CONF_PATH
+from virtme_ng.utils import VERSION, CONF_PATH
 
 if sys.version_info < (3,8):
     print('virtme-ng requires Python 3.8 or higher')
@@ -20,11 +20,11 @@ setup(
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.md'), 'r').read(),
     long_description_content_type="text/markdown",
-    packages=['kernelcraft', 'virtme', 'virtme.commands', 'virtme.guest'],
+    packages=['virtme_ng', 'virtme', 'virtme.commands', 'virtme.guest'],
     install_requires=['argcomplete'],
     entry_points = {
         'console_scripts': [
-            'virtme-ng = kernelcraft.run:main',
+            'virtme-ng = virtme_ng.run:main',
             'virtme-run = virtme.commands.run:main',
             'virtme-configkernel = virtme.commands.configkernel:main',
             'virtme-mkinitramfs = virtme.commands.mkinitramfs:main',
