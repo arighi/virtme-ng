arighi/virtme-ng

commit 14c312da3e16ca738c0ef9d3c7c649bb798fc718

virtme: init process refactoring
Move all the commands required to initialize the system into
virtme-ng-init (or virtme-init).

Moreover, when running scripts inside the guest don't use /run as a
temporary place to store the script that needs to be executed, use /mnt
instead, because /run needs to be re-mounted with tmpfs by
virtme-ng-init or virtme-init.

This is the first step to provide an initramfs-less way of executing
scripts inside the guest, that should help to improve boot time even
more (for the script execution case).

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -730,6 +730,14 @@ def do_it() -> int:
     # Check if initramfs is required.
     need_initramfs = args.force_initramfs or qemu.cannot_overmount_virtfs
 
+    # NOTE: running a script requires initramfs at the moment, because the
+    # script to execute is created inside the initramfs.
+    #
+    # Later on we should figure out a way to run scripts directly without
+    # using an initramfs.
+    if args.script_sh or args.script_exec:
+        need_initramfs = True
+
     config = mkinitramfs.Config()
 
     if len(args.overlay_rwdir) > 0:
@@ -830,14 +838,19 @@ def do_it() -> int:
         virtme_init_cmd = "virtme-init"
 
     if args.root == "/":
-        initcmds = [f"exec {guest_tools_path}/{virtme_init_cmd}"]
+        initcmds = [f"init={guest_tools_path}/{virtme_init_cmd}"]
     else:
         export_virtfs(qemu, arch, qemuargs, guest_tools_path, "virtme.guesttools")
         initcmds = [
-            "mkdir -p /run/virtme/guesttools",
-            "/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any "
-            + "virtme.guesttools /run/virtme/guesttools",
-            f"exec /run/virtme/guesttools/{virtme_init_cmd}",
+            "init=/bin/sh",
+            "--",
+            "-c",
+            ";".join([
+                "mkdir -p /run/virtme/guesttools",
+                "/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any " + \
+                "virtme.guesttools /run/virtme/guesttools",
+                f"exec /run/virtme/guesttools/{virtme_init_cmd}",
+            ]),
         ]
 
     # Arrange for modules to end up in the right place
@@ -978,22 +991,9 @@ def do_it() -> int:
                 ]
             )
 
-    has_script = False
-
     def do_script(shellcmd: str, use_exec=False, show_boot_console=False) -> None:
         if args.graphics is not None:
             arg_fail("scripts and --graphics are mutually exclusive")
-
-        nonlocal has_script
-        nonlocal need_initramfs
-        if has_script:
-            arg_fail("conflicting script options")
-        has_script = True
-
-        # NOTE: running a script requires initramfs at the moment, in the
-        # future try to figure out a way to run scripts directly without
-        # generating an initramfs.
-        need_initramfs = True
 
         # Turn off default I/O
         qemuargs.extend(arch.qemu_nodisplay_args())
@@ -1194,7 +1194,6 @@ def do_it() -> int:
             ]
         )
         initrdpath = None
-        initcmds.insert(0, "mount -t tmpfs run /run")
 
     if not args.verbose:
         kernelargs.append("quiet")
@@ -1208,9 +1207,7 @@ def do_it() -> int:
     # because we're explicitly passing '--' to set the arguments directly.
     # Fortunately, 'init=' will clear any arguments parsed so far, so make
     # sure that 'init=' appears directly before '--'.
-    kernelargs.append("init=/bin/sh")
-    kernelargs.append("--")
-    kernelargs.extend(["-c", ";".join(initcmds)])
+    kernelargs.extend(initcmds)
 
     # Load a normal kernel
     qemuargs.extend(["-kernel", kernel.kimg])

virtme/guest/virtme-init
@@ -15,11 +15,15 @@ log() {
     fi
 }
 
+# Mount procfs and sysfs (needed for stat, sadly)
+mount -t proc -o nosuid,noexec,nodev proc /proc/
 mount -t sysfs -o nosuid,noexec,nodev sys /sys/
 
-# Setup rw filesystem overlays
+# Mount tmpfs dirs
 mount -t tmpfs tmpfs /tmp/
+mount -t tmpfs run /run/
 
+# Setup rw filesystem overlays
 for tag in "${!virtme_rw_overlay@}"; do
     dir="${!tag}"
     upperdir="/tmp/$tag/upper"
@@ -86,9 +90,6 @@ elif [[ -x /lib/systemd/systemd-udevd ]]; then
 else
     udevd=`which udevd`
 fi
-
-# Mount proc (needed for stat, sadly)
-mount -t proc -o nosuid,noexec,nodev proc /proc/
 
 # devtmpfs might be automounted; if not, mount it.
 if ! grep -q devtmpfs /proc/mounts; then
@@ -222,7 +223,7 @@ if cat /proc/cmdline |grep -q -E '(^| )virtme.snapd($| )'; then
     fi
 fi
 
-if [[ -x /run/virtme/data/script ]]; then
+if [[ -x /mnt/virtme/data/script ]]; then
     if [[ ! -e "/dev/virtio-ports/virtme.stdin" ||
           ! -e "/dev/virtio-ports/virtme.stdout" ||
           ! -e "/dev/virtio-ports/virtme.stderr" ||
@@ -250,7 +251,7 @@ if [[ -x /run/virtme/data/script ]]; then
 
     # Start the script
     log 'starting script'
-    setsid /run/virtme/data/script </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
+    setsid /mnt/virtme/data/script </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
     log "script returned $?"
 
     # Hmm.  We should expose the return value somehow.

virtme/mkinitramfs.py
@@ -128,8 +128,8 @@ else
   umount /newroot/proc  # Don't leave garbage behind
 fi
 
-if ! [[ -d /newroot/run ]]; then
-  log "your guest's root does not have /run -- using a fallback tmpfs"
+if ! [[ -d /newroot/mnt ]]; then
+  log "your guest's root does not have /mnt -- using a fallback tmpfs"
   need_fallback_tmpfs=1
 fi
 
@@ -137,7 +137,7 @@ if [[ "$need_fallback_tmpfs" != "" ]]; then
   mount --move /newroot /tmproot
   mount -t tmpfs root_workaround /newroot/
   cd tmproot
-  mkdir /newroot/proc /newroot/sys /newroot/dev /newroot/run /newroot/tmp
+  mkdir /newroot/proc /newroot/sys /newroot/dev /newroot/run /newroot/mnt /newroot/tmp
   for i in *; do
     if [[ -d "$i" && \! -d "/newroot/$i" ]]; then
       mkdir /newroot/"$i"
@@ -149,8 +149,8 @@ if [[ "$need_fallback_tmpfs" != "" ]]; then
   umount -l /tmproot
 fi
 
-mount -t tmpfs run /newroot/run
-cp -a /run_virtme /newroot/run/virtme
+mount -t tmpfs mnt /newroot/mnt
+cp -a /run_virtme /newroot/mnt/virtme
 
 # Find init
 mount -t proc none /proc

virtme_ng_init/src/main.rs
@@ -40,13 +40,6 @@ struct MountInfo {
 
 const KERNEL_MOUNTS: &[MountInfo] = &[
     MountInfo {
-        source: "sys",
-        target: "/sys",
-        fs_type: "sysfs",
-        flags: libc::MS_NOSUID | libc::MS_NOEXEC | libc::MS_NODEV,
-        fsdata: "",
-    },
-    MountInfo {
         source: "proc",
         target: "/proc",
         fs_type: "proc",
@@ -54,8 +47,22 @@ const KERNEL_MOUNTS: &[MountInfo] = &[
         fsdata: "",
     },
     MountInfo {
+        source: "sys",
+        target: "/sys",
+        fs_type: "sysfs",
+        flags: libc::MS_NOSUID | libc::MS_NOEXEC | libc::MS_NODEV,
+        fsdata: "",
+    },
+    MountInfo {
         source: "tmpfs",
         target: "/tmp",
+        fs_type: "tmpfs",
+        flags: 0,
+        fsdata: "",
+    },
+    MountInfo {
+        source: "run",
+        target: "/run",
         fs_type: "tmpfs",
         flags: 0,
         fsdata: "",
@@ -633,7 +640,7 @@ fn run_user_script() {
 
         clear_virtme_envs();
         unsafe {
-            Command::new("/run/virtme/data/script")
+            Command::new("/mnt/virtme/data/script")
                 .pre_exec(move || {
                     nix::libc::setsid();
                     libc::close(libc::STDIN_FILENO);
@@ -877,7 +884,7 @@ fn main() {
 
     // Start user session (batch or interactive).
     set_cwd();
-    if utils::is_file_executable("/run/virtme/data/script") {
+    if utils::is_file_executable("/mnt/virtme/data/script") {
         run_user_script();
     } else {
         setup_user_session();

commit 80367daf1b2f860cacbea923a9574bec660c644b

virtme: properly mount tmpfs on /run when --root is used
When --root is used we are not mounting tmpfs in /run, causing a boot
failure. Make sure to add this mount as the first init command.

Fixes: 14c312d ("virtme: init process refactoring")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -838,6 +838,7 @@ def do_it() -> int:
             "--",
             "-c",
             ";".join([
+                "mount -t tmpfs run /run",
                 "mkdir -p /run/virtme/guesttools",
                 "/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any " +
                 "virtme.guesttools /run/virtme/guesttools",