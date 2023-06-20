dc8454085475ed3a5ccc5cef347629afbb3b3d23
Ricardo Ribalda Delgado <ricardo@ribalda.com>
2023-06-20 10:00:49 +0200

    debian: Add support for cross-compilation
    Use the debian cargo wrapper.
    
    [ arighi: adjust debian/patches/crossbuild.patch ]
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo@ribalda.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian/vendor-rust.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p virtme_ng_init/.cargo
-cat << EOF > virtme_ng_init/.cargo/config.toml
-[source]
-[source.debian-packages]
-directory = "/usr/share/cargo/registry"
-[source.crates-io]
-replace-with = "debian-packages"
-EOF
