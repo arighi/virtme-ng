arighi/virtme-ng

commit 18680daba86d713abd7530247490a1bc482ceddd

virtme-ng: pass --exec command via /proc/cmdline
Instead creating an initramfs on-the-fly to store the --exec command(s)
that need to be executed in the guest, pass them to the guest via
/proc/cmdline, using the kernel boot parameter virtme.exec=`<command>`.

This allows to save some extra boot time, because we don't have to
create an initramfs on-the-fly on the host and use it to boot the guest.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -730,14 +730,6 @@ def do_it() -> int:
     # Check if initramfs is required.
     need_initramfs = args.force_initramfs or qemu.cannot_overmount_virtfs
 
-    # NOTE: running a script requires initramfs at the moment, because the
-    # script to execute is created inside the initramfs.
-    #
-    # Later on we should figure out a way to run scripts directly without
-    # using an initramfs.
-    if args.script_sh or args.script_exec:
-        need_initramfs = True
-
     config = mkinitramfs.Config()
 
     if len(args.overlay_rwdir) > 0:
@@ -1049,16 +1041,7 @@ def do_it() -> int:
         qemuargs.extend(["-no-reboot"])
 
         # Ask virtme-init to run the script
-        config.virtme_data[
-            b"script"
-        ] = """#!/bin/sh
-
-        {prefix}{shellcmd}
-        """.format(
-            shellcmd=shellcmd, prefix="exec " if use_exec else ""
-        ).encode(
-            "ascii"
-        )
+        kernelargs.append(f"virtme.exec=`{shellcmd}`")
 
         # Nasty issue: QEMU will set O_NONBLOCK on fds 0, 1, and 2.
         # This isn't inherently bad, but it can cause a problem if

virtme/guest/virtme-init
@@ -223,7 +223,8 @@ if cat /proc/cmdline |grep -q -E '(^| )virtme.snapd($| )'; then
     fi
 fi
 
-if [[ -x /mnt/virtme/data/script ]]; then
+user_cmd=$(sed -ne "s/.*virtme.exec=\`\(.*\)\`.*/\1/p" /proc/cmdline)
+if [[ -n "${user_cmd}" ]]; then
     if [[ ! -e "/dev/virtio-ports/virtme.stdin" ||
           ! -e "/dev/virtio-ports/virtme.stdout" ||
           ! -e "/dev/virtio-ports/virtme.stderr" ||
@@ -251,7 +252,7 @@ if [[ -x /mnt/virtme/data/script ]]; then
 
     # Start the script
     log 'starting script'
-    setsid /mnt/virtme/data/script </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
+    setsid bash -c "$user_cmd" </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
     log "script returned $?"
 
     # Hmm.  We should expose the return value somehow.

virtme/mkinitramfs.py
@@ -128,30 +128,6 @@ else
   umount /newroot/proc  # Don't leave garbage behind
 fi
 
-if ! [[ -d /newroot/mnt ]]; then
-  log "your guest's root does not have /mnt -- using a fallback tmpfs"
-  need_fallback_tmpfs=1
-fi
-
-if [[ "$need_fallback_tmpfs" != "" ]]; then
-  mount --move /newroot /tmproot
-  mount -t tmpfs root_workaround /newroot/
-  cd tmproot
-  mkdir /newroot/proc /newroot/sys /newroot/dev /newroot/run /newroot/mnt /newroot/tmp
-  for i in *; do
-    if [[ -d "$i" && \! -d "/newroot/$i" ]]; then
-      mkdir /newroot/"$i"
-      mount --bind "$i" /newroot/"$i"
-    fi
-  done
-  mknod /newroot/dev/null c 1 3
-  mount -o remount,ro -t tmpfs root_workaround /newroot
-  umount -l /tmproot
-fi
-
-mount -t tmpfs mnt /newroot/mnt
-cp -a /run_virtme /newroot/mnt/virtme
-
 # Find init
 mount -t proc none /proc
 for arg in `cat /proc/cmdline`; do

virtme_ng_init/src/main.rs
@@ -590,7 +590,22 @@ fn setup_network() -> Option<thread::JoinHandle<()>> {
     None
 }
 
-fn run_user_script() {
+fn extract_user_script(virtme_script: &str) -> Option<String> {
+    let start_marker = "virtme.exec=`";
+    let end_marker = "`";
+
+    if let Some(start_index) = virtme_script.find(start_marker) {
+        let start_index = start_index + start_marker.len();
+        if let Some(end_index) = virtme_script[start_index..].find(end_marker) {
+            let cmd = &virtme_script[start_index..start_index + end_index];
+            return Some(cmd.to_string());
+        }
+    }
+
+    None
+}
+
+fn do_run_user_script(cmd: &str) {
     if !std::path::Path::new("/dev/virtio-ports/virtme.stdin").exists()
         || !std::path::Path::new("/dev/virtio-ports/virtme.stdout").exists()
         || !std::path::Path::new("/dev/virtio-ports/virtme.stderr").exists()
@@ -640,7 +655,8 @@ fn run_user_script() {
 
         clear_virtme_envs();
         unsafe {
-            Command::new("/mnt/virtme/data/script")
+            Command::new("/bin/sh")
+                .args(["-c", cmd])
                 .pre_exec(move || {
                     nix::libc::setsid();
                     libc::close(libc::STDIN_FILENO);
@@ -657,6 +673,16 @@ fn run_user_script() {
                 .expect("Failed to execute script");
         }
     }
+}
+
+fn run_user_script() -> bool {
+    if let Ok(cmdline) = std::fs::read_to_string("/proc/cmdline") {
+        if let Some(cmd) = extract_user_script(&cmdline) {
+            do_run_user_script(&cmd);
+            return true;
+        }
+    }
+    return false;
 }
 
 fn setup_root_home() {
@@ -884,9 +910,7 @@ fn main() {
 
     // Start user session (batch or interactive).
     set_cwd();
-    if utils::is_file_executable("/mnt/virtme/data/script") {
-        run_user_script();
-    } else {
+    if !run_user_script() {
         setup_user_session();
         run_user_session();
     }

virtme_ng_init/src/utils.rs
@@ -98,19 +98,6 @@ pub fn do_symlink(src: &str, dst: &str) {
     }
 }
 
-pub fn check_file_permissions(file_path: &str, mask: u32) -> bool {
-    if let Ok(metadata) = std::fs::metadata(file_path) {
-        let permissions = metadata.permissions();
-        permissions.mode() & mask != 0
-    } else {
-        false
-    }
-}
-
-pub fn is_file_executable(file_path: &str) -> bool {
-    check_file_permissions(file_path, 0o111)
-}
-
 pub fn do_mount(source: &str, target: &str, fstype: &str, flags: u64, fsdata: &str) {
     let source_cstr = CString::new(source).expect("CString::new failed");
     let fstype_cstr = CString::new(fstype).expect("CString::new failed");

commit 94a3033161e22316a06aec6937f534292479885c

virtme-ng: encode shell command passed via --exec to base64
Commit 18680da ("virtme-ng: pass --exec command via /proc/cmdline")
introduced a regression found by the mutter test case [1].

Passing the --exec command via /proc/cmdline directly, instead of using
a temporary initramfs, is faster, but it has the downside that we don't
support special characters anymore (such as double quotes for example).

To solve this regression and avoid breaking the old behavior we can pass
the command as a base64-encoded string, that will be decoded in the
guest by virtme-ng-init/virtme-init.

The overhaed of the base64 encoding/decoding is negligible (definitely
better than creating a temporary initramfs) and we can use any special
character in the shell command.

This fixes issue #27.

[1] https://gitlab.gnome.org/GNOME/mutter/-/jobs/2917515

Fixes: 18680da ("virtme-ng: pass --exec command via /proc/cmdline")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -21,6 +21,7 @@ import subprocess
 import signal
 from shutil import which
 from time import sleep
+from base64 import b64encode
 from .. import virtmods
 from .. import modfinder
 from .. import mkinitramfs
@@ -1044,6 +1045,10 @@ def do_it() -> int:
 
         # Scripts shouldn't reboot
         qemuargs.extend(["-no-reboot"])
+
+        # Encode the shell command to base64 to handle special characters (such
+        # as quotes, double quotes, etc.).
+        shellcmd = b64encode(shellcmd.encode('utf-8')).decode('utf-8')
 
         # Ask virtme-init to run the script
         kernelargs.append(f"virtme.exec=`{shellcmd}`")

virtme/guest/virtme-init
@@ -250,9 +250,12 @@ if [[ -n "${user_cmd}" ]]; then
     ln -s /dev/virtio-ports/virtme.dev_stdout /dev/stdout
     ln -s /dev/virtio-ports/virtme.dev_stderr /dev/stderr
 
+    # Decode shell command (base64) and dump it to a script
+    echo $user_cmd | base64 -d > /tmp/.virtme-script
+
     # Start the script
     log 'starting script'
-    setsid bash -c "$user_cmd" </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
+    setsid bash /tmp/.virtme-script </dev/virtio-ports/virtme.stdin >/dev/virtio-ports/virtme.stdout 2>/dev/virtio-ports/virtme.stderr
     log "script returned $?"
 
     # Hmm.  We should expose the return value somehow.