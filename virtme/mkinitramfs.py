arighi/virtme-ng

commit 918afa53357cd8e250e6983f28f1e86bc808c83a

mkinitramfs: support zstd compressed kernel modules
Busybox does not seem to support zstd-compressed kernel modules, so make
sure to uncompress them before they are added to the initramfs.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/mkinitramfs.py
@@ -8,7 +8,8 @@
 from typing import List, Dict, Optional
 
 import io
-import os.path
+import os
+import tempfile
 import shlex
 from . import cpiowriter
 from . import util
@@ -89,11 +90,17 @@ def install_modprobe(cw):
 def install_modules(cw, modfiles):
     cw.mkdir(b"modules", 0o755)
     paths = []
-    for mod in modfiles:
-        with open(mod, "rb") as f:
-            modpath = "modules/" + os.path.basename(mod)
-            paths.append(modpath)
-            cw.write_file(name=modpath.encode("ascii"), body=f, mode=0o644)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        for mod in modfiles:
+            if mod.endswith('.zst'):
+                mod_file = os.path.basename(mod)
+                uncompressed_mod = tmpdirname + os.path.splitext(mod_file)[0]
+                os.system(f"zstd -d < {mod} > {uncompressed_mod}")
+                mod = uncompressed_mod
+            with open(mod, "rb") as f:
+                modpath = "modules/" + os.path.basename(mod)
+                paths.append(modpath)
+                cw.write_file(name=modpath.encode("ascii"), body=f, mode=0o644)
 
     script = _LOGFUNC + "\n".join(
         "log 'loading %s...'; insmod %s" % (os.path.basename(p), shlex.quote(p))

commit 0e5c315f42e0c71ba0fad18fa2a504bdcb7b06c3

virtme: properly clean up temporary uncompressed module files
Properly clean up temporary module files when creating an initramfs with
zstd-compressed modules.

Fixes: 918afa5 mkinitramfs: support zstd compressed kernel modules
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/mkinitramfs.py
@@ -94,7 +94,7 @@ def install_modules(cw, modfiles):
         for mod in modfiles:
             if mod.endswith('.zst'):
                 mod_file = os.path.basename(mod)
-                uncompressed_mod = tmpdirname + os.path.splitext(mod_file)[0]
+                uncompressed_mod = tmpdirname + '/' + os.path.splitext(mod_file)[0]
                 os.system(f"zstd -d < {mod} > {uncompressed_mod}")
                 mod = uncompressed_mod
             with open(mod, "rb") as f: