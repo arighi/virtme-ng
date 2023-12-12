arighi/virtme-ng

commit 654a9286e873ce3aca367b3cc5a93b219fecc807

virtme: restore terminal settings on exit
Stopping virtme-ng from an external signal (e.g., SIGTERM) can leave the
terminal in a bad state (i.e., key strokes not echoed back to the
terminal and similar).

Example reproducer:

 $ timeout 1 vng -- sleep 5

Fix this by saving the terminal state before starting the qemu session
and restore the saved state on exit.

This fixes issue #49.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -19,6 +19,8 @@ import re
 import itertools
 import subprocess
 import signal
+import termios
+import tty
 from shutil import which
 from time import sleep
 from base64 import b64encode
@@ -1259,12 +1261,31 @@ def do_it() -> int:
             os.execv(qemu.qemubin, qemuargs)
     return 0
 
+def save_terminal_settings():
+    try:
+        return termios.tcgetattr(sys.stdin)
+    except:
+        return None
+
+def restore_terminal_settings(settings):
+    if settings is not None:
+        termios.tcsetattr(sys.stdin, termios.TCSAFLUSH, settings)
+
+def signal_handler(signum, frame):
+    sys.exit(1)
+
 
 def main() -> int:
+    # Catch potential signals that may interrupt the execution (SIGTERM) and
+    # make sure the terminal settings are restored on exit.
+    settings = save_terminal_settings()
+    signal.signal(signal.SIGTERM, signal_handler)
     try:
         return do_it()
     except SilentError:
         return 1
+    finally:
+        restore_terminal_settings(settings)
 
 
 if __name__ == "__main__":

commit 05f61b6498d6c6882852bd7c468a3a395e3ef442

virtme: use isatty() to check if stdin is a valid terminal
Use sys.stdin.isatty() to determine if we need to save/resume the
terminal state or not.

This seems more reliable than catching potential exceptions from
termios.

Fixes: 654a928 ("virtme: restore terminal settings on exit")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/run.py
@@ -1267,10 +1267,7 @@ def do_it() -> int:
 
 
 def save_terminal_settings():
-    try:
-        return termios.tcgetattr(sys.stdin)
-    except termios.error:
-        return None
+    return termios.tcgetattr(sys.stdin) if sys.stdin.isatty() else None
 
 
 def restore_terminal_settings(settings):