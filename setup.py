arighi/virtme-ng

commit 3da9eb91ae52ef8d0d56ddbfb88e33eff6d4130a

setup.py: fix logic around the virtme_ng_init submodule
First, os.listdir("virtme_ng_init") throws an exception when the
directory doesn't exist, which is not desirable. Replace it with
os.path.exists(), which does the right thing.

Second, it only makes sense to check for the virtme_ng_init submodule
when BUILD_VIRTME_NG_INIT != 0, so add it to the conditional.

Signed-off-by: Ondrej Mosnáček <omosnacek@gmail.com>

setup.py
@@ -20,16 +20,16 @@ if os.path.exists(".config"):
             key, value = line.strip().split("=")
             os.environ[key] = value
 
+# Global variables to store custom build options (as env variables)
+build_virtme_ng_init = int(os.environ.get("BUILD_VIRTME_NG_INIT", 0))
+
 # Make sure virtme-ng-init submodule has been cloned
-if not os.listdir("virtme_ng_init"):
+if build_virtme_ng_init and not os.path.exists("virtme_ng_init"):
     sys.stderr.write("WARNING: virtme-ng-init submoule not available, trying to clone it\n")
     check_call("git submodule update --init --recursive", shell=True)
 
 # Always include standard site-packages to PYTHONPATH
 os.environ['PYTHONPATH'] = sysconfig.get_paths()['purelib']
-
-# Global variables to store custom build options (as env variables)
-build_virtme_ng_init = int(os.environ.get("BUILD_VIRTME_NG_INIT", 0))
 
 
 def is_arm_32bit():

commit 259c1dd9f215d6f230333d767398f8e87f031536

setup.py: properly detect when the virtme-ng-init submodule is missing
Check for the Cargo.toml file in the virtme-ng-init submodule to
determine if the submodule is missing.

Checking for the directory is not enough: even if the submodule is not
downloaded the directory still exists (as an empty dir).

Fixes: 3da9eb9 ("setup.py: fix logic around the virtme_ng_init submodule")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

setup.py
@@ -24,7 +24,7 @@ if os.path.exists(".config"):
 build_virtme_ng_init = int(os.environ.get("BUILD_VIRTME_NG_INIT", 0))
 
 # Make sure virtme-ng-init submodule has been cloned
-if build_virtme_ng_init and not os.path.exists("virtme_ng_init"):
+if build_virtme_ng_init and not os.path.exists("virtme_ng_init/Cargo.toml"):
     sys.stderr.write("WARNING: virtme-ng-init submodule not available, trying to clone it\n")
     check_call("git submodule update --init --recursive", shell=True)
 