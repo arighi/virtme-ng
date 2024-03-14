arighi/virtme-ng

commit bd763bf5c42ecd21a3c3416cc692abb7e036deae

virtme-ng-init: docker host support
Minor changes required to run virtme-ng instances inside docker
containers.

Link: https://github.com/arighi/virtme-ng/issues/51
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme_ng_init/src/main.rs
@@ -411,6 +411,12 @@ fn mount_kernel_modules() {
     let kver = get_kernel_version(false);
     let mod_dir = format!("/lib/modules/{}", kver);
 
+    // Make sure to always have /lib/modules, otherwise we won't be able to configure kmod support
+    // properly (this can happen in some container environments, such as docker).
+    if !Path::new(&mod_dir).exists() {
+        utils::do_mkdir("/lib/modules");
+    }
+
     if env::var("virtme_root_mods").is_ok() {
         // /lib/modules is already set up.
     } else if let Ok(dir) = env::var("virtme_link_mods") {
@@ -673,9 +679,12 @@ fn setup_user_script(uid: u32) {
 }
 
 fn setup_root_home() {
-    utils::do_mkdir("/tmp/roothome");
-    utils::do_mount("/tmp/roothome", "/root", "", libc::MS_BIND as usize, "");
-    env::set_var("HOME", "/tmp/roothome");
+    // Set up a basic environment (unless virtme-ng is running as root on the host)
+    if let Err(_) = env::var("virtme_root_user") {
+        utils::do_mkdir("/tmp/roothome");
+        utils::do_mount("/tmp/roothome", "/root", "", libc::MS_BIND as usize, "");
+        env::set_var("HOME", "/tmp/roothome");
+    }
 }
 
 fn clear_virtme_envs() {

commit ae75ff3b82f86f5fbe54d5936dbcd109a186ac52

init: set the HOME env var if root
When virtme-ng was running as root, the HOME dir was set to '/'.

Now it is set to '/root', the expected value.

Fixes: bd763bf ("virtme-ng-init: docker host support")
Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

virtme_ng_init/src/main.rs
@@ -753,6 +753,8 @@ fn setup_root_home() {
         utils::do_mkdir("/tmp/roothome");
         utils::do_mount("/tmp/roothome", "/root", "", libc::MS_BIND as usize, "");
         env::set_var("HOME", "/tmp/roothome");
+    } else {
+        env::set_var("HOME", "/root");
     }
 }
 