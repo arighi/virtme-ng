arighi/virtme-ng

commit 83095fe95210201c87ef02da1a42c6f0ab5884be

configkernel: add --configitem and --verbose
add --configitem CONFIG_FOO=bar option

We already have --custom <file>, which loads one or more multi-item
config fragments.  With this you can set up a reliable baseline build,
without having to "make menuconfig" additions and modifications.

  $> virtme-configkernel --defconfig --custom baseline

The --configitem complements this with single CONFIG_FOO=BAR
additions.  They are applied after the --custom <file>s, and override
previous settings.

So you can specify a base-config with 1 or 2 --custom <file>s, and
then specify a bunch of alt-configs with a clear purpose, not with the
config hidden in the details of layering of a bunch of fragment files.

  $> virtme-configkernel --defconfig --custom baseline \
		--configitem CONFIG_LOCALVERSION="-leak" \
  		--configitem CONFIG_KEMLEAK=y

  $> virtme-configkernel --defconfig --custom baseline \
  		--configitem CONFIG_LOCALVERSION="-ksan" \
		--configitem CONFIG_KASAN=y

  # also accepts items wo CONFIG_ prefix

  $> virtme-configkernel --defconfig --custom baseline \
  		--configitem LOCALVERSION="-leaksan" \
		--configitem KEMLEAK=y \
		--configitem KASAN=y

NOTES

I wanted --config-item, but didnt want to fiddle with dash vs _under.

You can also use a --configitem free-expression as a probe to
understand what the kernel make config overrides mean, and where your
CONFIG_ITEMs are amongst the many coming from _GENERIC_CONFIG* and
elsewhere.

 $ virtme-configkernel --verbose --defconfig \
     --custom ../../../kfrag.dyndbg \
     --configitem CONFIG_KVM=p \
     --configitem CONFIG_TEST_DYNAMIC_DEBUG=y

the make produces a long-long list of overrides, but the "KVM=p"
elicits a particular "error" in addition to the override warning.

This provides a visually distinct location in the enormous output.

.config:5433:warning: override: reassigning to symbol KVM_GUEST
.config:5434:warning: override: reassigning to symbol KVM

virtme/commands/configkernel.py
@@ -39,9 +39,22 @@ def make_parser():
     )
 
     parser.add_argument(
+        "--configitem",
+        action="append",
+        metavar="CONFITEM",
+        help="add or alter a (CONFIG_)?FOO=bar item",
+    )
+
+    parser.add_argument(
         "--no-update",
         action="store_true",
         help="Skip if the config file already exists",
+    )
+
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="get chatty about config assembled",
     )
 
     g = parser.add_argument_group(title="Mode").add_mutually_exclusive_group()
@@ -271,13 +284,31 @@ def do_it():
             with open(conf_chunk, "r", encoding="utf-8") as fd:
                 custom_conf += fd.readlines()
 
+    if args.verbose:
+        print(f"custom:\n{custom_conf}")
+
+    mod_conf = []
+    if args.configitem:
+        mod_conf += [ f"# final config-item mods" ]
+        for conf_item in args.configitem:
+            if not conf_item.startswith("CONFIG_"):
+                conf_item = "CONFIG_" + conf_item
+            mod_conf += [ conf_item ]
+
+    if args.verbose:
+        print(f"mods:\n{mod_conf}")
+
     conf = (
         _GENERIC_CONFIG_OPTIONAL
         + ["# Arch-specific options"]
         + arch.config_base()
         + custom_conf
+        + mod_conf
         + _GENERIC_CONFIG
     )
+
+    if args.verbose:
+        print(f"conf:\n{conf}")
 
     linuxname = shlex.quote(arch.linuxname)
     archargs = [f"ARCH={linuxname}"]

commit ef3f5155086cc3376a7c844e6e7e2549999090ed

virtme-configkernel: fix coding style
Fix a couple of pylint errors. No functional change.

Fixes: 83095fe ("configkernel: add --configitem and --verbose")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/commands/configkernel.py
@@ -289,11 +289,11 @@ def do_it():
 
     mod_conf = []
     if args.configitem:
-        mod_conf += [ f"# final config-item mods" ]
+        mod_conf += ["# final config-item mods"]
         for conf_item in args.configitem:
             if not conf_item.startswith("CONFIG_"):
                 conf_item = "CONFIG_" + conf_item
-            mod_conf += [ conf_item ]
+            mod_conf += [conf_item]
 
     if args.verbose:
         print(f"mods:\n{mod_conf}")