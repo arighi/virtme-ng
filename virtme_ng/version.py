arighi/virtme-ng

commit 5cdb2f0035ded07e836352862a9b2f908d183f5c

version: generate verison string based on git information
If virtme-ng is installed from the git repository add git information to
the version string.

If, for any reason, we fail to determine the git information (e.g., when
building virtme-ng from a distro source package), fallback to the main
packaging version, typically updated manually every time we cut a new
release.

Suggested-by: Jim Cromie <jim.cromie@gmail.com>
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme_ng/version.py
@@ -3,7 +3,39 @@
 
 """virtme-ng version"""
 
-VERSION = "1.25"
+from subprocess import check_output, DEVNULL, CalledProcessError
 
-if __name__ == '__main__':
+PKG_VERSION = "1.25"
+
+
+def get_version_string():
+    try:
+        # Get the version from git describe
+        version = (
+            check_output(
+                "git describe --always --long --dirty",
+                shell=True,
+                stderr=DEVNULL,
+            )
+            .decode("utf-8")
+            .strip()
+        )
+
+        # Remove the 'v' prefix if present
+        if version.startswith("v"):
+            version = version[1:]
+
+        # Replace hyphens with plus sign for build metadata
+        version_pep440 = version.replace("-", "+", 1).replace("-", ".")
+
+        return version_pep440
+    except CalledProcessError:
+        # Default version if git describe fails (e.g., when building virtme-ng
+        # from a source package.
+        return PKG_VERSION
+
+
+VERSION = get_version_string()
+
+if __name__ == "__main__":
     print(VERSION)

commit 0ded79aa68576fd92c35033498b8771a160dda53

version: run git describe from the path of the python file
When we append the git information to the version string we can't simply
run git describe from any location, or we may end up getting the version
of another git repository (like the kernel).

To prevent this, always run `git describe` from the path where
version.py is located: installed vesions will return the plain version
as before, while running `vng --version` directly from the git repo will
return the version with the additional git information.

Fixes: 5cdb2f0 ("version: generate verison string based on git information")
Signed-off-by: Andrea Righi <righi.andrea@gmail.com>

virtme_ng/version.py
@@ -3,6 +3,7 @@
 
 """virtme-ng version"""
 
+import os
 from subprocess import check_output, DEVNULL, CalledProcessError
 
 PKG_VERSION = "1.25"
@@ -13,7 +14,7 @@ def get_version_string():
         # Get the version from git describe
         version = (
             check_output(
-                "git describe --always --long --dirty",
+                "cd %s && git describe --always --long --dirty" % os.path.dirname(__file__),
                 shell=True,
                 stderr=DEVNULL,
             )