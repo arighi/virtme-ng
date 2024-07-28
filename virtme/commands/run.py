arighi/virtme-ng

commit 4bf382a4e63f68ef543b60d5f42c470164c9cf62

virtme: pass "debug" to the boot options when running in verbose mode
Some distro / kernels require the "debug" boot option to properly show
the kernel messages to the console.

Add this parameter to correctly show the kernel log when "-v" is used.

This allows to properly show the kernel log in Arch Linux, CachyOS (and
probably most of the Arch-derivative distro).

Signed-off-by: Andrea Righi <righi.andrea@gmail.com>

virtme/commands/run.py
@@ -1063,7 +1063,7 @@ def do_it() -> int:
             qemuargs.extend(["-chardev", "file,path=/proc/self/fd/2,id=dmesg"])
             qemuargs.extend(["-device", arch.virtio_dev_type("serial")])
             qemuargs.extend(["-device", "virtconsole,chardev=dmesg"])
-            kernelargs.extend(["console=hvc0"])
+            kernelargs.extend(["console=ttyS0", "debug"])
 
             # Unfortunately we can't use hvc0 to redirect early console
             # messages to stderr, so just send them to the main console, in

commit 6758f55dc187ef2880d86a3dc1a53144fe60510c

virtme: properly show/redirect kernel log when running in command mode
Properly show/redirect kernel log (adding the "debug" boot option) also
when running in non-interactive mode, for example:

 $ vng -v -- uname -r 2>/tmp/boot.log

Fixes: 4bf382a ("virtme: pass "debug" to the boot options when running in verbose mode")
Signed-off-by: Andrea Righi <righi.andrea@gmail.com>

virtme/commands/run.py
@@ -1063,7 +1063,7 @@ def do_it() -> int:
             qemuargs.extend(["-chardev", "file,path=/proc/self/fd/2,id=dmesg"])
             qemuargs.extend(["-device", arch.virtio_dev_type("serial")])
             qemuargs.extend(["-device", "virtconsole,chardev=dmesg"])
-            kernelargs.extend(["console=ttyS0", "debug"])
+            kernelargs.extend(["console=ttyS0"])
 
             # Unfortunately we can't use hvc0 to redirect early console
             # messages to stderr, so just send them to the main console, in
@@ -1410,6 +1410,8 @@ def do_it() -> int:
     if not args.verbose:
         kernelargs.append("quiet")
         kernelargs.append("loglevel=0")
+    else:
+        kernelargs.append("debug")
 
     # Now that we're done setting up kernelargs, append user-specified args
     # and then initargs