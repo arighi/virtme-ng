arighi/virtme-ng

commit 4c81de766d1bf1a41c88c06a9a66a536e12caf41

virtme: architectures: Add "image" as s390x image name
This commit enables to use vng -r on a s390x machine.

Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

virtme/architectures.py
@@ -408,6 +408,9 @@ class Arch_s390x(Arch):
         return ["-device", "sclpconsole,chardev=console"]
 
 
+    def img_name(self) -> str:
+        return "image"
+
 ARCHES = {
     arch.virtmename: arch
     for arch in [

commit cb9c204fad31c5b8da9123824ab0f09130c0f2b9

virtme: fix lint warnings in architectures.py
Fix the following lint warnings:

 virtme/architectures.py:415:5: E303 too many blank lines (2)
 virtme/architectures.py:418:1: E305 expected 2 blank lines after class or function definition, found 1

Fixes: 4c81de7 ("virtme: architectures: Add "image" as s390x image name")
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

virtme/architectures.py
@@ -411,9 +411,9 @@ class Arch_s390x(Arch):
     def qemu_serial_console_args():
         return ["-device", "sclpconsole,chardev=console"]
 
-
     def img_name(self) -> str:
         return "image"
+
 
 ARCHES = {
     arch.virtmename: arch