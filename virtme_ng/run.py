arighi/virtme-ng

commit e12dfb2914a40eb989dc4a12ba4c181f754e1a72

Add cross-compile argument
When cross-compiling on SUSE distributions vng fails to find gcc, since
it's named '<arch>-suse-linux-gcc', instead of the hardcoded
'<arch>-linux-gnu-gcc'.

This commit provides a '--cross-compile' argument that will override the
hardcoded prefix if used, otherwise it should do nothing.

Signed-off-by: Fredrik Lönnegren <fredrik@frelon.se>

virtme_ng/run.py
@@ -443,6 +443,12 @@ virtme-ng is based on virtme, written by Andy Lutomirski <luto@kernel.org>.
     )
 
     parser.add_argument(
+        "--cross-compile",
+        action="store",
+        help="Set cross-compile prefix"
+    )
+
+    parser.add_argument(
         "--force",
         action="store_true",
         help="Force reset git repository to target branch or commit "
@@ -733,6 +739,9 @@ class KernelSource:
                 arg_fail(f"unsupported architecture: {arch}")
             target = ARCH_MAPPING[arch]["kernel_target"]
             cross_compile = ARCH_MAPPING[arch]["cross_compile"]
+            if args.cross_compile != "":
+                cross_compile=args.cross_compile
+
             cross_arch = ARCH_MAPPING[arch]["linux_name"]
         else:
             target = "bzImage"

virtme/commands/run.py
@@ -205,6 +205,12 @@ def make_parser() -> argparse.ArgumentParser:
         help="Guest architecture",
     )
     g.add_argument(
+        "--cross-compile",
+        action="store",
+        metavar="CROSS_COMPILE_PREFIX",
+        help="Cross-compile compiler prefix",
+    )
+    g.add_argument(
         "--busybox",
         action="store",
         metavar="PATH_TO_BUSYBOX",

virtme/commands/configkernel.py
@@ -32,6 +32,13 @@ def make_parser():
     )
 
     parser.add_argument(
+        "--cross-compile",
+        action="store",
+        metavar="CROSS_COMPILE_PREFIX",
+        help="Cross-compile compiler prefix",
+    )
+
+    parser.add_argument(
         "--custom",
         action="append",
         metavar="CUSTOM",
@@ -313,9 +320,13 @@ def do_it():
     linuxname = shlex.quote(arch.linuxname)
     archargs = [f"ARCH={linuxname}"]
 
-    if shutil.which(f"{arch.gccname}-linux-gnu-gcc") and arch.gccname != uname.machine:
-        gccname = shlex.quote(f"{arch.gccname}-linux-gnu-")
-        archargs.append(f"CROSS_COMPILE={gccname}")
+    cross_compile_prefix = f"{arch.gccname}-linux-gnu-"
+    if args.cross_compile != "":
+        cross_compile_prefix = args.cross_compile
+
+    if shutil.which(f"{cross_compile_prefix}-gcc") and arch.gccname != uname.machine:
+        gccname = shlex.quote(cross_compile_prefix)
+        archargs.append(f"CROSS_COMPILE={cross_compile_prefix}")
 
     maketarget: Optional[str]
 

commit 2fd590c39e948594239ee9af15399c16db1b91d4

virtme-ng: fix incorrect cross-compile override
Properly check if the --cross-compile option has been specified, so that
we can correctly use the built-in cross-compile prefixes.

This fixes the cross-architecture builds without using the
--cross-compile option.

Fixes: e12dfb2 ("Add cross-compile argument")
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

virtme_ng/run.py
@@ -752,7 +752,7 @@ class KernelSource:
                 arg_fail(f"unsupported architecture: {arch}")
             target = ARCH_MAPPING[arch]["kernel_target"]
             cross_compile = ARCH_MAPPING[arch]["cross_compile"]
-            if args.cross_compile != "":
+            if args.cross_compile:
                 cross_compile = args.cross_compile
 
             cross_arch = ARCH_MAPPING[arch]["linux_name"]