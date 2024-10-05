arighi/virtme-ng

commit 97dd475e88cc5de528a8ce7c5014b671b8427991

Add hint when trying to create root filesystem outside a Debian host
The user can specify a directory that does not exist with --root and the
tool will create one for him. However, it depends on `lsb_release` which is
not available in non-Debian hosts. Therefore, add a hint so the user knows
what to do.

Signed-off-by: Ricardo B. Marliere <rbm@suse.com>

virtme_ng/run.py
@@ -542,11 +542,15 @@ def create_root(destdir, arch, release):
     # Use Ubuntu's cloud images to create a rootfs, these images are fairly
     # small and they provide a nice environment to test kernels.
     if release is None:
-        release = (
-            check_output("lsb_release -s -c", shell=True)
-            .decode(sys.stdout.encoding)
-            .rstrip()
-        )
+        try:
+            release = (
+                check_output("lsb_release -s -c", shell=True)
+                .decode(sys.stdout.encoding)
+                .rstrip()
+            )
+        except CalledProcessError as e:
+            print("Try specifying an Ubuntu release with --root-release")
+            raise e
     url = (
         "https://cloud-images.ubuntu.com/"
         + f"{release}/current/{release}-server-cloudimg-{arch}-root.tar.xz"

commit f79d5c3ec54be8b4f976f432e3fb8363c30ddc80

virtme-ng: handle undefined release with --root
When the --root option is used to automatically create a new chroot from
scratch, virtme-ng attempts to populate the chroot using the latest
Ubuntu cloud image.

The specific Ubuntu release is determined by running lsb_release on the
host system, aiming to match the host environment as closely as
possible. However, if the host is not running Ubuntu, this command may
either fail or return "n/a" (e.g., on Arch Linux).

In such cases, treat the "n/a" result as an error and prompt the user to
manually specify the target Ubuntu release for initializing the chroot.

Fixes: 97dd475 ("Add hint when trying to create root filesystem outside a Debian host")
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

virtme_ng/run.py
@@ -548,9 +548,11 @@ def create_root(destdir, arch, release):
                 .decode(sys.stdout.encoding)
                 .rstrip()
             )
-        except CalledProcessError as e:
-            print("Try specifying an Ubuntu release with --root-release")
-            raise e
+            if release == "n/a":
+                raise ValueError("unknown release")
+        except (CalledProcessError, ValueError):
+            print("Unknown release, try specifying an Ubuntu release with --root-release")
+            sys.exit(1)
     url = (
         "https://cloud-images.ubuntu.com/"
         + f"{release}/current/{release}-server-cloudimg-{arch}-root.tar.xz"