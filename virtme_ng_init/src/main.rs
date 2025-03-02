arighi/virtme-ng

commit da5b5867d2882c71c738578b2af4e7f146a76a37

virtme-ng: export real /tmp to guest
Use /run to store overlays instead of /tmp.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

virtme/guest/virtme-init
@@ -20,14 +20,13 @@ mount -t proc -o nosuid,noexec,nodev proc /proc/
 mount -t sysfs -o nosuid,noexec,nodev sys /sys/
 
 # Mount tmpfs dirs
-mount -t tmpfs tmpfs /tmp/
 mount -t tmpfs run /run/
 
 # Setup rw filesystem overlays
 for tag in "${!virtme_rw_overlay@}"; do
     dir="${!tag}"
-    upperdir="/tmp/$tag/upper"
-    workdir="/tmp/$tag/work"
+    upperdir="/run/tmp/$tag/upper"
+    workdir="/run/tmp/$tag/work"
     mkdir -p "$upperdir" "$workdir"
     mnt_opts="xino=off,lowerdir=$dir,upperdir=$upperdir,workdir=$workdir"
     mount -t overlay -o "${mnt_opts}" "${tag}" "${dir}" &

virtme/commands/run.py
@@ -447,11 +447,6 @@ def find_kernel_and_mods(arch, args) -> Kernel:
             # the modules, just rely on /lib/modules in the target rootfs.
             if root_dir == "/" or args.root != '/':
                 kernel.use_root_mods = True
-            elif root_dir.startswith("/tmp"):
-                sys.stderr.write(
-                    "\nWarning: /tmp is hidden inside the guest, "
-                    + "kernel modules won't be supported at runtime unless you move them somewhere else.\n\n"
-                )
             kernel.moddir = f"{root_dir}/lib/modules/{kver}"
             if not os.path.exists(kernel.moddir):
                 kernel.modfiles = []

virtme_ng/run.py
@@ -798,7 +798,7 @@ class KernelSource:
         else:
             self.virtme_param["overlay_rwdir"] = " ".join(
                 f"--overlay-rwdir {d}"
-                for d in ("/etc", "/lib", "/home", "/opt", "/srv", "/usr", "/var")
+                for d in ("/etc", "/lib", "/home", "/opt", "/srv", "/usr", "/var", "/tmp")
             )
         # Add user-specified overlays.
         for item in args.overlay_rwdir:

commit ca47a26024fe1bcb82207d733d065a156591d8a6

init: clean up previous X11 state
When running vng in graphic mode (-g), recent versions of Xorg may fail
to start with the following error:

 _XSERVTransSocketUNIXCreateListener: ...SocketCreateListener() failed
 _XSERVTransMakeAllCOTSServerListeners: server already running
 (EE)
 Fatal server error:
 (EE) Cannot establish any listening sockets - Make sure an X server isn't already running(EE)

This can happen because the host's /tmp is exposed to the guest and Xorg
might get confused finding a previous state.

Fix by removing the Xorg state files before starting the X server in the
vng guest.

Fixes: da5b586 ("virtme-ng: export real /tmp to guest")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

virtme/guest/virtme-init
@@ -440,6 +440,9 @@ if [[ -n "${virtme_graphics}" ]]; then
         pre_exec_cmd=""
     fi
 
+    # Clean up any previous X11 state.
+    rm -f /tmp/.X11*/* /tmp/.X11-lock
+
     # Create a .xinitrc to start the requested graphical application.
     xinit_rc=/run/tmp/.xinitrc
     echo -e "${pre_exec_cmd}\nexec /run/tmp/.virtme-script" > ${xinit_rc}

virtme_ng_init/src/main.rs
@@ -921,6 +921,9 @@ fn run_user_gui(tty_fd: libc::c_int) {
         // directly here so we may need extra permissions on the tty devices.
         utils::run_cmd("bash", &["-c", &format!("chown {} /dev/char/*", user)]);
 
+        // Clean up any previous X11 state.
+        utils::run_cmd("bash", &["-c", &"rm -f /tmp/.X11*/* /tmp/.X11-lock"]);
+
         // Start xinit directly.
         storage = format!("su {} -c 'xinit /run/tmp/.xinitrc'", user);
         args.push(&storage);