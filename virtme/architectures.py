arighi/virtme-ng

commit f3749da339ace58ef9fcf6aed1e65d4b35f90067

virtme-ng: redirect kernel log to stderr in interactive mode
Redirect kernel messages to stderr when running in interactive mode,
similar to what we do in command/script mode.

In this way it is possible, for example, to save kernel logs to a file
even when running in interactive mode, such as:

 $ vng -vr 2>/tmp/kernel.log

NOTE: unfortunately we can't redirect early boot kernel messages,
earlyprintk doesn't seem to work with virconsole devices. So, early
messages will be still sent to stdout, in this way we don't break the
old behavior.

This addresses discussion #60.

Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

virtme/architectures.py
@@ -124,7 +124,7 @@ class Arch_x86(Arch):
 
     @staticmethod
     def serial_console_args():
-        return ["console=ttyS0"]
+        return ["virtme_console=ttyS0"]
 
     @staticmethod
     def config_base():
@@ -225,7 +225,7 @@ class Arch_arm(Arch):
 
     @staticmethod
     def serial_console_args():
-        return ["console=ttyAMA0"]
+        return ["virtme_console=ttyAMA0"]
 
     def kimg_path(self):
         return "arch/arm/boot/zImage"
@@ -274,7 +274,7 @@ class Arch_aarch64(Arch):
 
     @staticmethod
     def serial_console_args():
-        return ["console=ttyAMA0"]
+        return ["virtme_console=ttyAMA0"]
 
     def kimg_path(self):
         return "arch/arm64/boot/Image"
@@ -334,7 +334,7 @@ class Arch_riscv64(Arch):
 
     @staticmethod
     def serial_console_args():
-        return ["console=ttyS0"]
+        return ["virtme_console=ttyS0"]
 
     def kimg_path(self):
         return "arch/riscv/boot/Image"

virtme/commands/run.py
@@ -986,22 +986,37 @@ def do_it() -> int:
     qemuargs.extend(["-net", "none"])
 
     if args.graphics is None and not args.script_sh and not args.script_exec:
-        # It would be nice to use virtconsole, but it's terminally broken
-        # in current kernels.  Nonetheless, I'm configuring the console
-        # manually to make it easier to tweak in the future.
         qemuargs.extend(["-echr", "1"])
         qemuargs.extend(["-serial", "none"])
-        qemuargs.extend(["-chardev", "stdio,id=console,signal=off,mux=on"])
-
-        qemuargs.extend(arch.qemu_serial_console_args())
-
-        qemuargs.extend(["-mon", "chardev=console"])
 
         if args.verbose:
+            # Check if we have permission to access the current stderr.
+            if not can_access_file("/proc/self/fd/2"):
+                print(
+                    "ERROR: not a valid pts, try to run vng inside tmux or screen",
+                    file=sys.stderr,
+                )
+                sys.exit(1)
+
+            # Redirect kernel messages to stderr, creating a separate console
+            qemuargs.extend(["-chardev", "file,path=/proc/self/fd/2,id=dmesg"])
+            qemuargs.extend(["-device", arch.virtio_dev_type("serial")])
+            qemuargs.extend(["-device", "virtconsole,chardev=dmesg"])
+            kernelargs.extend(["console=hvc0"])
+
+            # Unfortunately we can't use hvc0 to redirect early console
+            # messages to stderr, so just send them to the main console, in
+            # this way we don't lose early printk's in verbose mode and we can
+            # catch potential boot issues.
             kernelargs.extend(arch.earlyconsole_args())
-        qemuargs.extend(arch.qemu_nodisplay_args())
+
+        qemuargs.extend(["-chardev", "stdio,id=console,signal=off,mux=on"])
+        qemuargs.extend(["-serial", "chardev:console"])
+        qemuargs.extend(["-mon", "chardev=console"])
 
         kernelargs.extend(arch.serial_console_args())
+
+        qemuargs.extend(arch.qemu_nodisplay_args())
 
         # PS/2 probing is slow; give the kernel a hint to speed it up.
         kernelargs.extend(["psmouse.proto=exps"])

virtme/guest/virtme-init
@@ -310,7 +310,11 @@ if [[ -n "${user_cmd}" ]]; then
 fi
 
 # Figure out what the main console is
-consdev="`grep ' ... (.C' /proc/consoles  |cut -d' ' -f1`"
+if [[ -n "${virtme_console}" ]]; then
+    consdev=${virtme_console}
+else
+    consdev="`grep ' ... (.C' /proc/consoles  |cut -d' ' -f1`"
+fi
 if [[ -z "$consdev" ]]; then
     log "can't deduce console device"
     exec bash --login  # At least try to be helpful
@@ -334,6 +338,10 @@ if [[ ! -e "/dev/$consdev" ]]; then
     log "/dev/$consdev doesn't exist."
     exec bash --login
 fi
+
+# Redirect current stdout/stderr to consdev
+exec 1>/dev/${consdev}
+exec 2>&1
 
 # Parameters that start with virtme_ shouldn't pollute the environment
 for p in "${!virtme_@}"; do export -n "$p"; done

commit dc0864a0281355da2c46d7ecab12beb1e489860d

architectures: s390x: Fix serial console args
Use 'ttysclp0' (SCLP VT220) as the console, this fixes the the STDIN/STDOUT. See
[1] for details about device and console names on s390x.

[1] https://www.ibm.com/docs/en/linux-on-systems?topic=wysk-device-console-names

Fixes: f3749da339ac ("virtme-ng: redirect kernel log to stderr in interactive mode")
Fixes: 19c7f3958767 ("virtme: always print kernel panic / oops in interactive mode")
Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>

virtme/architectures.py
@@ -410,6 +410,10 @@ class Arch_s390x(Arch):
         return ["CONFIG_MARCH_Z900=y"]
 
     @staticmethod
+    def serial_console_args() -> List[str]:
+        return ["ttysclp0"]
+
+    @staticmethod
     def qemu_serial_console_args():
         return ["-device", "sclpconsole,chardev=console"]
 