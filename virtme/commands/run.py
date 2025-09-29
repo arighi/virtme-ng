arighi/virtme-ng

commit 2290a6a7e3c4834f93aaa9795f69bafb0807ab05

commands: run.py: Resolve symlinks on get_kernel_version
In distributions like openSUSE/SLE, vmlinuz files can be just symlinks
pointing to the real file elsewhere, like the example below:

$ file /boot/vmlinuz
/boot/vmlinuz: symbolic link to vmlinuz-6.16.3-1-default

In such cases, the detection of the kernel version may fail to find
the kernel version, and thus not setting up the kernel modules, making
the boot to fail. This happens because one of the detections rely on
the output of the file command, which shows the kernel version if the
file is a regular file:

$ file /usr/lib/modules/6.16.3-1-default/vmlinuz | grep version
/usr/lib/modules/6.16.3-1-default/vmlinuz: Linux kernel x86 boot executable,
bzImage, version 6.16.3-1-default...

Now, if the user runs vng -r /boot/vmlinuz, it can fail because the output
of the file command is different:

$ file /boot/vmlinuz
/boot/vmlinuz: symbolic link to vmlinuz-6.16.3-1-default

The fix resolves the symlink, so vng can have the output it wants. It
doesn't change the rationale of the detect, just avoids one corner case.

Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

virtme/commands/run.py
@@ -20,6 +20,7 @@ import sys
 import tempfile
 import termios
 from base64 import b64encode
+from pathlib import Path
 from shutil import which
 from time import sleep
 from typing import Any, Dict, List, NoReturn, Optional, Tuple
@@ -451,7 +452,10 @@ def get_rootfs_from_kernel_path(path):
     return os.path.abspath(path)
 
 
-def get_kernel_version(path, img_name: Optional[str] = None):
+def get_kernel_version(orig_path, img_name: Optional[str] = None):
+    # Resolve symlinks first
+    path = Path(orig_path).resolve()
+
     if not os.path.exists(path):
         arg_fail(f"kernel file {path} does not exist, try --build to build the kernel")
     if not os.access(path, os.R_OK):

commit df4514a98f1e70661e33b2a007919fee6741dceb

virtme/commands/run: Fix TypeError
`re.search(...)` expects a string as the second argument, therefore
let's convert the Path into a string.

  $ ./vng -r
  Traceback (most recent call last):
    File "/home/mhartmay/storage/git/virtme-ng/virtme-run", line 18, in <module>
      sys.exit(run.main())
               ^^^^^^^^^^
    File "/home/mhartmay/storage/git/virtme-ng/virtme/commands/run.py", line 2028, in main
      return do_it()
             ^^^^^^^
    File "/home/mhartmay/storage/git/virtme-ng/virtme/commands/run.py", line 1198, in do_it
      kernel = find_kernel_and_mods(arch, args)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    File "/home/mhartmay/storage/git/virtme-ng/virtme/commands/run.py", line 575, in find_kernel_and_mods
      kver = get_kernel_version(kimg, img_name)
             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    File "/home/mhartmay/storage/git/virtme-ng/virtme/commands/run.py", line 520, in get_kernel_version
      match = re.search(rf"{img_name}-({version_pattern})", path)
              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    File "/usr/lib/python3.12/re/__init__.py", line 177, in search
      return _compile(pattern, flags).search(string)
             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  TypeError: expected string or bytes-like object, got 'PosixPath'

Fixes: 2290a6a7e3c4 ("commands: run.py: Resolve symlinks on get_kernel_version")
Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>

virtme/commands/run.py
@@ -488,10 +488,10 @@ def get_kernel_version(orig_path, img_name: Optional[str] = None):
     # The version detection fails s390x using file or strings tools, so check
     # if the file itself contains the version number.
     if img_name is not None:
-        match = re.search(rf"{img_name}-({version_pattern})", path)
+        match = re.search(rf"{img_name}-({version_pattern})", str(path))
         if match:
             return match.group(1)
-        match = re.search(rf"/lib/modules/({version_pattern})/{img_name}", path)
+        match = re.search(rf"/lib/modules/({version_pattern})/{img_name}", str(path))
         if match:
             return match.group(1)
 