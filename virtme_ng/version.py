d5005ace0b2294456ca5f63ab48cf997e5ae9c3a
Andrea Righi <arighi@nvidia.com>
2025-11-30 09:32:15 +0100

    virtme-ng v1.39
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- virtme_ng/version.py
+++ virtme_ng/version.py
@@ -7,7 +7,7 @@ import os
 from importlib.metadata import PackageNotFoundError, version
 from subprocess import DEVNULL, CalledProcessError, check_output
 
-PKG_VERSION = "1.38"
+PKG_VERSION = "1.39"
 
 
 def get_package_version():
