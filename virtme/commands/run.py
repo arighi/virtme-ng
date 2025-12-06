644124da448bdc1c125b678f92227a827aba63e2
Ivan Shapovalov <intelfx@intelfx.name>
2025-12-06 20:50:23 +0100

    vng, run: truncate scsi device_id and serial if the full path does not fit
    The scsi-hd `serial=` parameter is used by QEMU as a default for the
    `device_id=` parameter (which is what in fact ends up in the by-id
    path).
    
    The `serial=` parameter is set by `virtme-run` using the disk "name"
    provided in the `--{,blk-}disk` argument, which is in turn set by `vng`
    to the disk path, when not explicitly provided.
    
    When all of this is chained, the full path to disk image ends up being
    used as the SCSI device_id:
    
    1. `vng --disk /path/to/disk` leads to
    2. `virtme-run --disk /path/to/disk=/path/to/disk` leads to
    3. `qemu -device scsi-hd,serial=/path/to/disk` leads to (internally)
    4. `qemu -device scsi-hd,serial=/path/to/disk,device_id=/path/to/disk`
    
    However, QEMU (or perhaps it is SCSI) limits the device_id to
    20 characters:
    
    ```
    qemu-system-x86_64: -device scsi-hd,drive=disk0,vendor=virtme,product=disk,serial=/long/path/to/image/file: The serial number can't be longer than 20 characters when it is also used as the default for device_id
    ```
    
    Likewise, the serial itself is limited to 36 characters.
    
    This problem is indirectly exposed since the previous commit, because
    `vng --disk` is now a shorthand for a SCSI disk, not a virtio-blk disk.
    
    Fix this by truncating the device path manually and using the truncated
    values as the SCSI serial and device_id.
    
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- virtme/commands/run.py
+++ virtme/commands/run.py
@@ -40,6 +40,7 @@ from virtme_ng.utils import (
     VIRTME_SSH_DESTINATION_NAME,
     VIRTME_SSH_HOSTNAME_CID_SEPARATORS,
     get_conf,
+    scsi_device_id,
 )
 
 from .. import architectures, mkinitramfs, modfinder, qemu_helpers, resources, virtmods
@@ -1843,6 +1844,12 @@ def do_it() -> int:
             driveid = f"disk{i}"
             disk = DiskArg.parse("--scsi-disk", d)
 
+            # scsi-hd.device_id= is normally defaulted to scsi-hd.serial=,
+            # but it must not be longer than 20 characters
+            device_id = scsi_device_id(disk.name, 20)
+            # scsi-hd.serial= itself must not be longer than 36 characters
+            serial = scsi_device_id(disk.name, 36)
+
             drive_opts = [
                 "if=none",
                 f"id={driveid}",
@@ -1853,7 +1860,8 @@ def do_it() -> int:
                 f"drive={driveid}",
                 "vendor=virtme",
                 "product=disk",
-                f"serial={disk.name}",
+                f"serial={serial}",
+                f"device_id={device_id}" if device_id != serial else None,
             ]
 
             qemuargs.extend(
