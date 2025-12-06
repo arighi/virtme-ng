840c3a1b88692fb428641c71eb0199e3d5d605cf
Ivan Shapovalov <intelfx@intelfx.name>
2025-12-06 20:39:44 +0100

    vng, run: support specifying / autodetecting disk emulation options
    This implements support for various disk topology and I/O driver options
    for both `--disk` and `--blk-disk` arguments.
    
    Options are accepted as a comma-separated list after the image file path
    e.g., `--disk /path/to/file,format=qcow2,...`.
    
    In addition to general QEMU options (format=), I/O driver options
    (cache=, aio=, discard=, detect-zeroes=, queues=) and topology options
    (log-sec=, phy-sec=, min-io=, opt-io=, disc-gran=) a "topology="
    metaoption is accepted to pass through host device queue limits.
    
    The names for these options were chosen to match `lsblk` columns
    rather than QEMU's own -drive/-device options, because the latter are
    underdocumented and non-uniform.
    
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- virtme_ng/run.py
+++ virtme_ng/run.py
@@ -428,8 +428,11 @@ virtme-ng is based on virtme, written by Andy Lutomirski <luto@kernel.org>.
         "--scsi-disk",
         action="append",
         default=[],
-        metavar="PATH",
-        help="Add a file as virtio-scsi disk (can be used multiple times)",
+        metavar="[NAME=]PATH[,OPT...]",
+        help="Add a file as virtio-scsi disk (can be used multiple times), "
+        + "available at /dev/disk/by-id/scsi-0virtme_disk_NAME. "
+        + "OPTs are comma-separated KEY[=VALUE] pairs; "
+        + "pass 'help' to list them.",
     )
 
     parser.add_argument(
@@ -437,8 +440,11 @@ virtme-ng is based on virtme, written by Andy Lutomirski <luto@kernel.org>.
         "-D",
         action="append",
         default=[],
-        metavar="PATH",
-        help="Add a file as virtio-blk disk (can be used multiple times)",
+        metavar="[NAME=]PATH[,OPT...]",
+        help="Add a file as virtio-blk disk (can be used multiple times), "
+        + "available at /dev/disk/by-id/virtio-virtme_disk_blk_NAME. "
+        + "OPTs are comma-separated KEY[=VALUE] pairs; "
+        + "pass 'help' to list them.",
     )
 
     parser.add_argument(
@@ -1227,8 +1233,17 @@ class KernelSource:
 
     def _get_virtme_disk(self, args):
         def ensure_name(dsk: str) -> str:
-            if "=" not in dsk:
-                return f"{dsk}={dsk}"
+            """
+            `dsk` is a comma-separated list of disk options (KEY=VAL), with the first
+            option specifying the disk name and path (NAME=PATH). As an exception,
+            NAME can be omitted (but the underlying implementation does not know that).
+            This function ensures that the first option has a NAME, and adds one
+            equal to the PATH if it is missing.
+            """
+            items = dsk.split(",")
+            first = items[0]
+            if "=" not in first:
+                return f"{first}={dsk}"
             return dsk
 
         disk_str = ""
