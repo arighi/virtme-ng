arighi/virtme-ng

commit 71de7b52ba541dc9c40e99e9c255fb72fb532ecf

virtme_ng_init: Add multiple missing log statements
Add multiple missing log statements in the Rust version of
`virtme-init`. This makes it easier to compare the Rust with the Bash
version.

Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>

virtme_ng_init/src/main.rs
@@ -263,6 +263,7 @@ fn configure_limits() {
 
 fn configure_hostname() {
     if let Ok(hostname) = env::var("virtme_hostname") {
+        log!("Setting hostname to {hostname}...");
         if let Err(err) = sethostname(hostname) {
             log!("failed to change hostname: {}", err);
         }
@@ -275,6 +276,7 @@ fn run_systemd_tmpfiles() {
     if !Path::new("/etc/systemd").exists() {
         return;
     }
+    log!("running systemd-tmpfiles");
     let args: &[&str] = &[
         "--create",
         "--boot",
@@ -347,6 +349,7 @@ fn generate_lvm() -> io::Result<()> {
 
     // Create a default lvm.conf file if --systemd was specified
     if id() != 1 {
+        log!("Creating a default lvm.conf file");
         let args: &[&str] = &["--type", "default", "--file", "/etc/lvm/lvm.conf"];
         utils::run_cmd("lvmconfig", args);
     }
@@ -558,7 +561,7 @@ fn disable_uevent_helper() {
 
     if Path::new(uevent_helper_path).exists() {
         // This kills boot performance.
-        log!("you have CONFIG_UEVENT_HELPER on, turn it off");
+        log!("you have CONFIG_UEVENT_HELPER on; turn it off");
         let mut file = OpenOptions::new().write(true).open(uevent_helper_path).ok();
         match &mut file {
             Some(file) => {
@@ -762,6 +765,7 @@ fn run_user_script(uid: u32) {
             ("su", vec!["-c", USER_SCRIPT, "--", user.as_str()])
         };
         clear_virtme_envs();
+        log!("starting script");
         unsafe {
             let ret = Command::new(cmd)
                 .args(&args)
@@ -1129,6 +1133,7 @@ fn main() {
     mount_sys_filesystems();
     mount_kernel_modules();
     run_systemd_tmpfiles();
+    log!("basic initialization done");
 
     // Service running in the background for later
     setup_socat_console();

commit 954d730931166db75be0fbf5bcabaaa4eb5ce616

virtme-ng-init: Mount /dev before logging
Ensure that mount_kernel_filesystems() is called before
configure_hostname() to guarantee that /dev (specifically /dev/kmsg) is
available before any code attempts to use the log!() macro.

Fix by swapping the order: run mount_kernel_filesystems() before
configure_hostname().

Fixes: 71de7b5 ("virtme_ng_init: Add multiple missing log statements")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

virtme_ng_init/src/main.rs
@@ -1125,8 +1125,8 @@ fn print_logo() {
 fn main() {
     // Basic system initialization (order is important here).
     configure_environment();
-    configure_hostname();
     mount_kernel_filesystems();
+    configure_hostname();
     mount_cgroupfs();
     configure_limits();
     mount_virtme_overlays();