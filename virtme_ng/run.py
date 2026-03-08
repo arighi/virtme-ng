arighi/virtme-ng

commit 8665b2bdba5bbebb29f4b0470b260be3b51be569

qmp: move option handling from vng to virtme
That was the only QEMU option that was directly handled from vng, while
all others are handled on virtme-run side.

Move it there with the others, that's clearer. Now 'vng' simply call
virtme-run with --qmp.

No behavioural changes intended here.

Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

virtme_ng/run.py
@@ -1302,6 +1302,12 @@ class KernelSource:
         else:
             self.virtme_param["gdb"] = ""
 
+    def _get_virtme_qmp(self, args):
+        if args.debug:
+            self.virtme_param["qmp"] = "--qmp"
+        else:
+            self.virtme_param["qmp"] = ""
+
     def _get_virtme_snaps(self, args):
         if args.snaps:
             self.virtme_param["snaps"] = "--snaps"
@@ -1339,14 +1345,8 @@ class KernelSource:
             self.virtme_param["nvgpu"] = ""
 
     def _get_virtme_qemu_opts(self, args):
-        qemu_args = ""
-        if args.debug or args.pin:
-            # Enable debug mode and QMP (to trigger memory dump via `vng --dump`)
-            qemu_args += "-qmp tcp:localhost:3636,server,nowait "
         if args.qemu_opts is not None:
-            qemu_args += " ".join(args.qemu_opts)
-        if qemu_args != "":
-            self.virtme_param["qemu_opts"] = "--qemu-opts " + qemu_args
+            self.virtme_param["qemu_opts"] = "--qemu-opts " + " ".join(args.qemu_opts)
         else:
             self.virtme_param["qemu_opts"] = ""
 
@@ -1394,6 +1394,7 @@ class KernelSource:
         self._get_virtme_numa_distance(args)
         self._get_virtme_balloon(args)
         self._get_virtme_gdb(args)
+        self._get_virtme_qmp(args)
         self._get_virtme_snaps(args)
         self._get_virtme_empty_passwords(args)
         self._get_virtme_busybox(args)
@@ -1447,6 +1448,7 @@ class KernelSource:
             + f"{self.virtme_param['numa_distance']} "
             + f"{self.virtme_param['balloon']} "
             + f"{self.virtme_param['gdb']} "
+            + f"{self.virtme_param['qmp']} "
             + f"{self.virtme_param['snaps']} "
             + f"{self.virtme_param['busybox']} "
             + f"{self.virtme_param['nvgpu']} "

virtme/commands/run.py
@@ -131,6 +131,11 @@ def make_parser() -> argparse.ArgumentParser:
         help="Open a GDB server on TCP to connect to the guest",
     )
     g.add_argument(
+        "--qmp",
+        action="store_true",
+        help="Enable QMP (QEMU Machine Protocol) support on TCP port 3636",
+    )
+    g.add_argument(
         "--graphics",
         action="store",
         nargs="?",
@@ -1641,6 +1646,9 @@ def do_it() -> int:
     if args.gdb_server is not None:
         qemuargs.extend(["-gdb", f"tcp:localhost:{args.gdb_server}"])
         kernelargs.extend(["-a", "nokaslr"])
+
+    if args.qmp:
+        qemuargs.extend(["-qmp", "tcp:localhost:3636,server,nowait"])
 
     ret_path = None
 

commit 3beedfe9f86e1cf34335282c4f7df4156748b83d

virtme-ng: Fix vCPU pinning
Using --pin without --debug triggers the following warning, breaking
vCPU pinning:

  QMP connection failed after 5 attempts
  WARNING: Failed to pin vCPUs

The reason is that without --debug, QMP protocol isn't enabled and vng
isn't able to determine the PIDs of the vCPUS.

Implicitly enable QMP when --pin is used.

Fixes: 8665b2b ("qmp: move option handling from vng to virtme")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

virtme_ng/run.py
@@ -1319,7 +1319,8 @@ class KernelSource:
             self.virtme_param["gdb"] = ""
 
     def _get_virtme_qmp(self, args):
-        if args.debug:
+        # QMP is needed for --debug (GDB/monitor) and for --pin (query-cpus-fast).
+        if args.debug or args.pin:
             self.virtme_param["qmp"] = "--qmp"
         else:
             self.virtme_param["qmp"] = ""