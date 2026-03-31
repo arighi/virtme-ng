22670a65a1c1947b7ff2f5594b224cd43a4ef41d
Ivan Shapovalov <intelfx@intelfx.name>
2026-03-31 10:47:45 +0200

    vng: replace `--disk` with `--blk-disk`, deprecate the former
    Previously, `vng --disk` was translated into `virtme-run --blk-disk`,
    contrary to the help claiming that `vng --disk` adds a virtio-scsi disk.
    
    At the same time, `virtme-run --disk` (same-named parameter) was indeed
    creating a virtio-scsi disk, making it a three-way inconsistency.
    
    Deprecate `vng --disk` (preserving its actual behavior of adding a
    virtio-blk disk) and add a `vng --blk-disk` with unambiguous meaning.
    
    Signed-off-by: Ivan Shapovalov <intelfx@intelfx.name>

--- virtme_ng/run.py
+++ virtme_ng/run.py
@@ -405,12 +405,31 @@ virtme-ng is based on virtme, written by Andy Lutomirski <luto@kernel.org>.
         + "The last octet will be incremented for the next network devices.",
     )
 
+    # legacy alias (`vng --disk` and `virtme-run --disk` behavior is historically inconsistent)
     parser.add_argument(
         "--disk",
-        "-D",
+        # "-D",  # explicitly moved to `--blk-disk` to preserve behavior
         action="append",
+        default=[],
+        metavar="PATH",
+        help=argparse.SUPPRESS,
+    )
+
+    parser.add_argument(
+        "--scsi-disk",
+        action="append",
+        default=[],
         metavar="PATH",
         help="Add a file as virtio-scsi disk (can be used multiple times)",
+    )
+
+    parser.add_argument(
+        "--blk-disk",
+        "-D",
+        action="append",
+        default=[],
+        metavar="PATH",
+        help="Add a file as virtio-blk disk (can be used multiple times)",
     )
 
     parser.add_argument(
@@ -1181,13 +1200,12 @@ class KernelSource:
             self.virtme_param["remote_cmd"] = ""
 
     def _get_virtme_disk(self, args):
-        if args.disk is not None:
-            disk_str = ""
-            for dsk in args.disk:
-                disk_str += f"--blk-disk {dsk}={dsk} "
-            self.virtme_param["disk"] = disk_str
-        else:
-            self.virtme_param["disk"] = ""
+        disk_str = ""
+        for dsk in args.scsi_disk:
+            disk_str += f"--scsi-disk {dsk}={dsk} "
+        for dsk in args.blk_disk:
+            disk_str += f"--blk-disk {dsk}={dsk} "
+        self.virtme_param["disk"] = disk_str
 
     def _get_virtme_sound(self, args):
         if args.sound:
@@ -1628,6 +1646,13 @@ def do_it() -> int:
     """Main body."""
     argcomplete.autocomplete(_ARGPARSER)
     args = _ARGPARSER.parse_args()
+
+    if args.disk:
+        sys.stderr.write(
+            "warning: `--disk` is deprecated, use `--blk-disk` instead.\n"
+        )
+        args.blk_disk.extend(args.disk)
+        args.disk = []
 
     # Handle --mcp option early (it's a server mode, not a normal
     # operation).
